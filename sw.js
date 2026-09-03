016c1269ebae5783856acabd90784b013c9ab67b
abbasalmdll1994-netizen <abbasalmdll1994@gmail.com>
2026-09-04 02:38:10 +0330

    Refresh offline cache for partial payment improvements

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 // Bump VERSION whenever any precached page, script or icon changes.
-const VERSION = 'offline-20260904-13';
+const VERSION = 'offline-20260904-14';
 const BASE = new URL('./', self.location.href);
 const PREFIX = 'qarya-shell:' + BASE.pathname + ':';
 const CACHE = PREFIX + VERSION;
